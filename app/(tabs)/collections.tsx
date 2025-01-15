d044e508bfeab4fe8cab956986a761797b7eef72
kingjack05 <kingjack05@gmail.com>
2025-01-15 23:39:46 +0800

    bug: simple list flicker

--- app/(tabs)/collections.tsx
+++ app/(tabs)/collections.tsx
@@ -1,8 +1,34 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput } from "react-native";
 
 export default function Collections() {
+  const [items, setItems] = useState<string[]>([]);
+  const [text, setText] = useState("");
   return (
     <View>
+      {items.map((i, index) => (
+        <Text
+          // className="text-foreground"
+          style={{ color: "white" }}
+          key={index}
+        >
+          {i}
+        </Text>
+      ))}
+      <TextInput
+        style={{ color: "white" }}
+        // className="text-foreground"
+        value={text}
+        onChangeText={(val) => {
+          setText(val);
+        }}
+        onEndEditing={() => {
+          setItems((prev) => [...prev, text]);
+          setText("");
+        }}
+        placeholder="New bullet"
+        placeholderTextColor="hsl(0, 0%, 40%)"
+      />
       <Text className="text-foreground">Collections</Text>
     </View>
   );
